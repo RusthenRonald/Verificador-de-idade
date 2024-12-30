34023f2d537ee41e54ec6f31de635d15563c7c39
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-12-29 23:17:07 -0300

    converti para number

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var nasc = document.getElementById('txtano')
 var res = document.getElementById('res')
 var fsex = document.getElementsByName('fsex')
 //validação
-if (nasc.value.length==0 || nasc.value>ano_atual){
+if (nasc.value.length==0 || Number(nasc.value)>ano_atual){
     window.alert('Dados inválidos!')
 }
 else{
