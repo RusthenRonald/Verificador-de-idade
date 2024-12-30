47e4404112be82a7b716b70b3b0e769f8784e6da
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-12-29 23:14:20 -0300

    fazendo validação caso o usuário digite 0 ou vazio

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ var ano_atual=new Date().getFullYear()
 var nasc = document.getElementById('txtano')
 var res = document.getElementById('res')
 var fsex = document.getElementsByName('fsex')
+//validação
+if (nasc.value.length==0){
+    window.alert('Dados inválidos!')
+}
+else{
+    window.alert('Continue')
+}
 }
