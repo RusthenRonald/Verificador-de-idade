d947cdd48c1461c4cc34bbaae2cba6d8f59cc8fe
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-12-29 23:11:09 -0300

    botei dentro da função

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+function verificar(){
 var ano_atual=new Date().getFullYear()
 var nasc = document.getElementById('txtano')
 var res = document.getElementById('res')
 var fsex = document.getElementsByName('fsex')
+}
