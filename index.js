92f2c2d994016b572526a1613fbfa82647f5c479
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-12-29 23:29:59 -0300

    criei div img pelo js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var ano_atual=new Date().getFullYear()
 var nasc = document.getElementById('txtano')
 var res = document.getElementById('res')
 var fsex = document.getElementsByName('fsex')
+var img = document.createElement('img')
 //validação
 if (nasc.value.length==0 || Number(nasc.value)>ano_atual){
     window.alert('Dados inválidos!')
