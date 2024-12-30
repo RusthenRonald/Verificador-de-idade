954a1661c4d10d50b4fb429a668efd8c4291a71d
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2024-12-29 23:28:40 -0300

    fiz condições para saber se é bebe, jovem , adulto ou idoso

--- index.js
+++ index.js
@@ -10,11 +10,37 @@ if (nasc.value.length==0 || Number(nasc.value)>ano_atual){
 else{
     var idade = ano_atual-Number(nasc.value)
     var genero=''
+    //se for masculino
     if (fsex[0].checked){
         genero='Homem'
+        if (idade>=0 && idade <10){
+            //bebe
+        }
+        else if(idade>=10 && idade <30){
+            //jovem
+        }
+        else if (idade>=30 && idade <50){
+            //adulto
+        }
+        else{
+            //idoso
+        }
     }
+    //se for feminino
     else{
         genero='Mulher'
+        if (idade>=0 && idade <10){
+            //bebe
+        }
+        else if(idade>=10 && idade <30){
+            //jovem
+        }
+        else if (idade>=30 && idade <50){
+            //adulto
+        }
+        else{
+            //idoso
+        }
     }
     res.style.textAlign='center'
     res.innerHTML=`Identificado ${genero} com idade ${idade}`
